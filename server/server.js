d90d4b9395a650e29d275c3cf297ffa4ef8f6fd3
Steven Maloney <steven.maloney@gmail.com>
2021-03-10 15:22:49 -0600

    added mongoose stuff to serverjs file

--- server/server.js
+++ server/server.js
@@ -14,11 +14,19 @@ app.use(express.static('build'));
 app.use('/api/players', playersRouter);
 app.use('/api/pool', poolRouter);
 
+require('dotenv').config();
 // App Set //
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+const mongoose = require('mongoose');
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 
-
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
 
 /** Listen * */
 app.listen(PORT, () => {
