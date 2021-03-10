68d9dee59b864091ee48e05b6376ef799bdf1b71
sdeda1us <steven.maloney@gmail.com>
2021-03-10 16:01:35 -0600

    working on get route for mogo atlas

--- server/routes/pool.router.js
+++ server/routes/pool.router.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const Pool = require('../models/pool.model');
 
-
+router.get('/', (req, res) => {
+    Pool.find()
+        .then(pool => res.json(pool))
+        .catch(err => res.status(400));
+});
 
 
 module.exports = router;
