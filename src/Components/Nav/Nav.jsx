8791eb62b09f01a897ab78499dc3ab7f9ab19a08
Steven Maloney <steven.maloney@gmail.com>
2021-03-29 17:59:57 -0500

    spiffied up the nav bar

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {Typography} from '@material-ui/core';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import {Button, Grid, Typography} from '@material-ui/core';
+import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core/styles';
 import Login from '../Login/Login.jsx';
 import Logout from '../Logout/Logout.jsx';
 import {useSelector} from 'react-redux';
-
 
 //defining custom fonts
 const theme = createMuiTheme({
@@ -15,16 +14,36 @@ const theme = createMuiTheme({
       ].join(','),
     },});
 
+    const useStyles = makeStyles((theme) => ({
+        root: {
+          flexGrow: 1,
+        },
+        paper: {
+          padding: theme.spacing(2),
+          textAlign: 'center',
+          color: '#00004d',
+        },
+        button: {
+          padding: theme.spacing(2),
+          textAlign: 'center',
+          border: '1px solid #00004d',
+          color: '#00004d',
+        }
+      }));
+
 export default function Nav() {
     const loginReducer = useSelector(state => state.loginReducer);
+    const classes = useStyles();
 
     return (
         <ThemeProvider theme={theme}>
-            <Typography variant="h1">MEGAfantasy</Typography>
-            <Link to="/auction">Auction Page</Link>
-            {loginReducer.email ? <p>Logged In</p>:<p>Not logged in</p>}
-            <Login />
-            <Logout />
+            <Grid container justify='center'>
+                <Grid item xs={12} className={classes.paper}><Typography variant="h1">MEGAfantasy</Typography></Grid>
+                <Link to="/"><Button className={classes.button}><Typography>Home</Typography></Button></Link>
+                <Link to="/auction"><Button className={classes.button}><Typography>Auction Page</Typography></Button></Link>
+                {loginReducer.email ? <Logout /> :<Login />}
+            </Grid>
+            
         </ThemeProvider>
     )
 
